9a54f7e1706291157b3ebb3f686e7bf66e34d114
appdevsw <appdevsw@wp.pl>
2015-11-16 15:51:26 +0100

    descriptions

---
+++ unpackdeb.sh
@@ -0,0 +1,28 @@
+
+tmpdir=/tmp/
+
+debpkg=$(readlink -f $1)
+
+rm -r $tmpdir/unpack
+mkdir $tmpdir/unpack
+cd $tmpdir/unpack
+ar -x $debpkg
+mkdir ./deb
+cd deb
+tar -xf ../data.tar.xz
+
+
+for f in $(find . -type f)
+do
+  echo "Copy $f"
+  cp $f /$f
+  if [ $? -eq 0 ]; then
+    echo OK
+  else
+    echo FAIL
+  fi
+done
+
+glib-compile-schemas /usr/share/glib-2.0/schemas
+
+
